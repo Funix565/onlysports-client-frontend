12b9ff10e648bfed7de62ba35c74bcb37a732e92
Funix565 <denkin.dmitriy@gmail.com>
2023-01-26 20:00:20 +0200

    Replace localhost with onlysports-api

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -15,7 +15,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     */
 
     const getPosts = async () => {
-        const response = await fetch("http://localhost:3001/posts", {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` }
         });
@@ -24,7 +24,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     };
 
     const getUserPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/posts/${userId}/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` }
         });
