ca3c92cf935fd1d9ef612e6ae260504bea634194
Funix565 <denkin.dmitriy@gmail.com>
2023-06-22 23:25:03 +0300

    New setup for Trainer~Team feature
    - New Routes for new components
    - LocalizationProvider wrapper for mui DatePicker
    - enums for roles and page type

--- src/state/enums.js
+++ src/state/enums.js
@@ -1,5 +1,13 @@
 export const SelectedPage = {
     Home: 'home',
     Benefits: 'benefits',
-    ContactUs: 'contactus'
+    ContactUs: 'contactus',
+    OnlyHome: 'onlyhome',
+    Profile: 'profile',
+    Team: 'team'
+};
+
+export const Roles = {
+    Trainer: 'TRAINER',
+    User: 'USER'
 };
