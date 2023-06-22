c4aa23842d2cbe729e51cb6b8c2cfe65f2ed2b3a
Funix565 <denkin.dmitriy@gmail.com>
2023-06-22 23:29:01 +0300

    Friends feature updates
    - Conditional rendering for Trainer
    - Forbid adding yourself as a friend
    - TODOs and indentation

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,25 +1,51 @@
-import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { EmojiEvents, PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setFriends } from "state";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
+import { useEffect, useState } from "react";
 
-const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+const Friend = ({friendId, name, subtitle, userPicturePath}) => {
+    // TODO: It could be Trainer as well?
+    //  Or create similar component
+
+    // TODO: Wow, this looks incredibly complicated to separate pages based on roles :(
+    // Friend feature for User is everywhere but Trainer doesn't have friends. Can't add friends.
+    // It means that Post feature where I can add author is also ruined
+    // Perhaps now I should figure out how to track Role on almost every page/component
+    // And change features based on that: add friend or add member
+    // At the same time Trainer can't be Friend of User. So track this behaviour
+    // So, User can't add Trainer. And Trainer can't add other Trainer
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { _id } = useSelector((state) => state.user);
+    const {_id} = useSelector((state) => state.user);
     const token = useSelector((state) => state.token);
     const friends = useSelector((state) => state.user.friends);
 
-    const { palette } = useTheme();
+    const {palette} = useTheme();
     const primaryLight = palette.primary.light;
     const primaryDark = palette.primary.dark;
     const main = palette.neutral.main;
     const medium = palette.neutral.medium;
 
     const isFriend = friends.find((friend) => friend._id === friendId);
+    const [isTrainer, setIsTrainer] = useState(null);
+
+    // Try to find Trainer with this friendId.
+    // We have fewer Trainers, so it is ok to perform a query with the collection ???
+    const getTrainer = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/trainers/${friendId}`,
+            {
+                method: "GET",
+                headers: {Authorization: `Bearer ${token}`}
+            }
+        );
+        const data = await response.json();
+        setIsTrainer(data);
+    };
 
     const patchFriend = async () => {
         const response = await fetch(
@@ -33,47 +59,75 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
             }
         );
         const data = await response.json();
-        dispatch(setFriends({ friends: data }));
+        dispatch(setFriends({friends: data}));
     };
+
+    // TODO: Looks complicated and silly. How to implement in a smarter way?
+    useEffect(() => {
+        getTrainer();
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <FlexBetween>
             <FlexBetween gap="1rem">
-                <UserImage image={userPicturePath} size="55px" />
+                <UserImage image={userPicturePath} size="55px"/>
                 <Box
                     onClick={() => {
                         navigate(`/profile/${friendId}`);
                         navigate(0); // Dirty workaround. 04:44:30
                     }}
                 >
-                    <Typography
-                        color={main}
-                        variant="h5"
-                        fontWeight="500"
-                        sx={{
-                            "&:hover": {
-                                color: palette.primary.light,
-                                cursor: "pointer"
-                            }
-                        }}
-                    >
-                        {name}
-                    </Typography>
+                    {isTrainer ? (
+                        <Box display="flex" alignItems="center">
+                            <Typography
+                                color={main}
+                                variant="h5"
+                                fontWeight="500"
+                                sx={{
+                                    "&:hover": {
+                                        color: palette.primary.light,
+                                        cursor: "pointer"
+                                    }
+                                }}
+                            >
+                                {name}
+                            </Typography>
+                            <EmojiEvents sx={{color: palette.primary.main}}/>
+                        </Box>
+                    ) : (
+                        <Typography
+                            color={main}
+                            variant="h5"
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer"
+                                }
+                            }}
+                        >
+                            {name}
+                        </Typography>
+                    )}
                     <Typography color={medium} fontSize="0.75rem">
                         {subtitle}
                     </Typography>
                 </Box>
             </FlexBetween>
-            <IconButton
-                onClick={() => patchFriend()}
-                sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-            >
-                {isFriend ? (
-                    <PersonRemoveOutlined sx={{ color: primaryDark }} />
-                ) : (
-                    <PersonAddOutlined sx={{ color: primaryDark }} />
-                )}
-            </IconButton>
+            {/*Forbid adding yourself as a friend*/}
+            {/*No icon for Trainer if User sees Trainer post*/}
+            {friendId !== _id && !isTrainer && (
+                <IconButton
+                    onClick={() => patchFriend()}
+                    sx={{backgroundColor: primaryLight, p: "0.6rem"}}
+                >
+                    {isFriend ? (
+                        <PersonRemoveOutlined sx={{color: primaryDark}}/>
+                    ) : (
+                        <PersonAddOutlined sx={{color: primaryDark}}/>
+                    )}
+                </IconButton>
+            )}
         </FlexBetween>
     );
 };
--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -5,56 +5,71 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
 
-const FriendListWidget = ({ userId }) => {
-  const dispatch = useDispatch();
-  const { palette } = useTheme();
-  const token = useSelector((state) => state.token);
-  const friends = useSelector((state) => state.user.friends);
+// TODO: Trainer doesn't have friends, so DON'T show this component on home page if Trainer role
+const FriendListWidget = ({userId}) => {
 
-  /*
-      Displays not only friends of current logged in user.
-      Also if we click on someone else's profile.
-  */
-  const getFriends = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/users/${userId}/friends`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` }
-      }
+    // TODO: FriendList feature is completely broken ~perhaps only on ProfilePage
+    //  Mess in state. useSelector subscription and update
+    //  Firstly, it tries to render all friends from the current state.
+    //  Then `useEffect` executes, network request, dispatch setFriends.
+    //  This of course updates the friends in state.
+    //  And this somehow affects the Post component. Add/Remove is not in sync.
+    //  Why? Because when we visit ProfilePage we fill state with that profile friends
+    //  And of course that profile doesn't have itself as a friend
+
+    const dispatch = useDispatch();
+    const {palette} = useTheme();
+    const token = useSelector((state) => state.token);
+
+
+    // TODO: Try same approach as in Members. Don't use global state here
+    const friends = useSelector((state) => state.user.friends);
+
+    /*
+        Displays not only friends of current logged in user.
+        Also if we click on someone else's profile.
+    */
+    const getFriends = async () => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/users/${userId}/friends`,
+            {
+                method: "GET",
+                headers: {Authorization: `Bearer ${token}`}
+            }
+        );
+
+        const data = await response.json();
+        dispatch(setFriends({friends: data}));
+    };
+
+    useEffect(() => {
+        getFriends();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <WidgetWrapper>
+            <Typography
+                color={palette.neutral.dark}
+                variant="h5"
+                fontWeight="500"
+                sx={{mb: "1.5rem"}}
+            >
+                Friend List
+            </Typography>
+            <Box display="flex" flexDirection="column" gap="1.5rem">
+                {/*TODO: Question mark?*/}
+                {friends?.map((friend) => (
+                    <Friend
+                        key={friend._id}
+                        friendId={friend._id}
+                        name={`${friend.fullName}`}
+                        subtitle={friend.occupation}
+                        userPicturePath={friend.picturePath}
+                    />
+                ))}
+            </Box>
+        </WidgetWrapper>
     );
-    const data = await response.json();
-
-    dispatch(setFriends({ friends: data }));
-  };
-
-  useEffect(() => {
-    getFriends();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return (
-    <WidgetWrapper>
-      <Typography
-        color={palette.neutral.dark}
-        variant="h5"
-        fontWeight="500"
-        sx={{ mb: "1.5rem" }}
-      >
-        Friend List
-      </Typography>
-      <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
-          <Friend
-            key={friend._id}
-            friendId={friend._id}
-            name={`${friend.fullName}`}
-            subtitle={friend.occupation}
-            userPicturePath={friend.picturePath}
-          />
-        ))}
-      </Box>
-    </WidgetWrapper>
-  );
 };
 
 export default FriendListWidget;
